2c6f238ae62175714e1ead913cf92140abf1bf33
anssattar <anssattar33@gmail.com>
2024-10-04 02:03:08 +0500

    07 react-icon library

--- client/src/components/Dashboard/SideBar/Side-Bar.jsx
+++ client/src/components/Dashboard/SideBar/Side-Bar.jsx
@@ -1,14 +1,15 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../../store/auth";
+import { RxAvatar } from "react-icons/rx";
 
-export const SideBar = ({onSelectChat}) => {
+export const SideBar = ({ onSelectChat }) => {
   const { user, userAuthentication, userFriends } = useAuth();
-  const { friends,userData } = user
+  const { friends, userData } = user;
 
   useEffect(() => {
-    userAuthentication()
-    userFriends(userData?._id)
+    userAuthentication();
+    userFriends(userData?._id);
   }, [userData?._id]);
 
   return (
@@ -27,22 +28,20 @@ export const SideBar = ({onSelectChat}) => {
         </Link>
       </div> */}
       <div className="relative my-8">
-        <h2 className="text-white text-xl">{friends?.role == "counselor"? "Student'S":"Counselor'S"}</h2>
+        <h2 className="text-white text-xl">
+          {friends?.role == "counselor" ? "Student'S" : "Counselor'S"}
+        </h2>
         <ul className="max-h-52 overflow-y-scroll scrollbar-hide shadow-lg rounded-lg my-4">
           {friends?.friends?.map((items, index) => (
             <li
               className="my-4 text-white text-sm"
               key={index}
-              onClick={() => onSelectChat(items , userData?._id)}
+              onClick={() => onSelectChat(items, userData?._id)}
             >
-              <div className="flex items-center">
-                <img
-                  className="w-12 rounded-full mr-4"
-                  src="/src/assets/reactIcon.png"
-                  alt="demy profile"
-                />
+              <div className="flex items-center justify-start cursor-pointer gap-4">
+                <RxAvatar size={42} />
                 <div className="text-white">
-                  <p>{items?.name}</p>
+                  <p className="capitalize ">{items?.name}</p>
                   <span>last message</span>
                 </div>
               </div>
